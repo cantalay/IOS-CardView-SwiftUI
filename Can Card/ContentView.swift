93b13f054cd3d6f0ff8ea311612c969a54d55f04
CanT <taalaay@gmail.com>
2019-12-17 13:24:58 +0300

    Initial Commit

---
+++ Can Card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Can Card
+//
+//  Created by Can Talay on 17.12.2019.
+//  Copyright © 2019 Can Talay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
